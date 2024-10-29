ed9d63b10cd2487a6393db215e3e5d775790186d
Paul Kukowski <kukowskipaul@gmail.com>
2024-10-29 17:39:48 +0100

    Updated nvim config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local bufnr = vim.api.nvim_get_current_buf()
+vim.keymap.set("n", "<leader>a", function()
+  vim.cmd.RustLsp("codeAction") -- supports rust-analyzer's grouping
+  -- or vim.lsp.buf.codeAction() if you don't want grouping.
+end, { silent = true, buffer = bufnr })
+-- vim.keymap.set("n", "<leader>a", "<Plug>RustHoverAction")
